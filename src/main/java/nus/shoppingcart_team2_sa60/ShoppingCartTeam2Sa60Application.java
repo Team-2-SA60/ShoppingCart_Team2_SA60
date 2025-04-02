76aa5ef2f9f0dc81905762484ed439a21a6f30a6
kinsengchan <chan_kin_seng@hotmail.com>
2025-04-02 16:57:59 +0800

    Initial commit

---
+++ src/main/java/nus/shoppingcart_team2_sa60/ShoppingCartTeam2Sa60Application.java
@@ -0,0 +1,13 @@
+package nus.shoppingcart_team2_sa60;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartTeam2Sa60Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCartTeam2Sa60Application.class, args);
+    }
+
+}
---
+++ src/test/java/nus/shoppingcart_team2_sa60/ShoppingCartTeam2Sa60ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.shoppingcart_team2_sa60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartTeam2Sa60ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
