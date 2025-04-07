e2a13be3b5f391c00a52a586294a6d4a7049ec15
adriantlh <adrian.tan.li.huat@gmail.com>
2025-04-07 15:08:29 +0800

    Adding the random repos that have not been created yet.
    Adding some queries to the product repo that need to be tested in Application.

--- src/main/java/nus/shoppingcart_team2_sa60/repository/ProductRepository.java
+++ src/main/java/nus/shoppingcart_team2_sa60/repository/ProductRepository.java
@@ -1,9 +1,45 @@
 package nus.shoppingcart_team2_sa60.repository;
 
 import nus.shoppingcart_team2_sa60.model.Product;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    Product findByName(String name);
+
+    Product findByProductId(int productId);
+
+    Product findByProductName(String productName);
+
+    @Query("SELECT p" +
+            "FROM Product p" +
+            "ORDER BY name ")
+    public List<Product> findProductByName(); //sorting by Alphabetical, from A
+
+    @Query("SELECT p" +
+            "FROM Product p" +
+            "ORDER BY name DESC")
+    public List<Product> findProductByNameDesc(); //sorting by Alphabetical, from Z
+
+
+    @Query("SELECT p" +
+            "FROM Products p " +
+            "WHERE p.description LIKE '%n'" +
+            "ORDER BY name")
+    public List<Product> findProductByDescriptionN();
+
+    @Query("SELECT p" +
+            "FROM Products p " +
+            "WHERE p.description LIKE '%n'" +
+            "ORDER BY name DESC")
+    public List<Product> findProductByDescriptionNDESC();
+
+    @Query("Select p" +
+            "FROM Products p " +
+            "ORDER BY p.price")
+    public List<Product> findProductByPrice();
+
 }
