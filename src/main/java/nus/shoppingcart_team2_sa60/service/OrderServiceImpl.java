c7a3a2716111ec4c825c402bb34b1fce7ba4e41f
vegecloud <caiyun_yap@hotmail.com>
2025-04-09 16:09:18 +0800

    Updated order controller

--- src/main/java/nus/shoppingcart_team2_sa60/service/OrderServiceImpl.java
+++ src/main/java/nus/shoppingcart_team2_sa60/service/OrderServiceImpl.java
@@ -1,25 +1,26 @@
 package nus.shoppingcart_team2_sa60.service;
 
-import jakarta.transaction.Transactional;
 import nus.shoppingcart_team2_sa60.model.Order;
 import nus.shoppingcart_team2_sa60.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository oRepo;
 
     @Override
-    public List<Order> searchOrdersByCustomerId(int customerId, String filter) {
-        if (filter == null) {
-            return oRepo.findOrdersByCustomerId(customerId);
-        }
-        else return null;
+    public List<Order> searchOrdersByCustomerId(int customerId) {
+         return oRepo.findOrdersByCustomerId(customerId);
     }
 
+    @Override
+    public List<Order> searchOrdersByStatus(int customerId, String status) {
+        return oRepo.findOrdersByStatus(customerId, status);
+    }
 }
