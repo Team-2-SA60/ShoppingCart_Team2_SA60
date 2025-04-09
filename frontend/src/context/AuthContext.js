5ae51377b25986a40925a6d002f47ea737d1cb74
kinsengchan <chan_kin_seng@hotmail.com>
2025-04-09 14:00:06 +0800

    AuthContext for checking session in React

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import api from "../utilities/axios";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({children}) {
+    const [ customer, setCustomer ] = useState(null);
+
+    async function checkSession() {
+        await api.get("/check-session")
+        .then(res => {
+            setCustomer(res.data);
+        })
+        .catch(res => {
+            console.log("Invalid session: " + res.data);
+            setCustomer(null);
+        })
+    }
+
+    return (
+        <AuthContext.Provider value={{customer, setCustomer, checkSession}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
