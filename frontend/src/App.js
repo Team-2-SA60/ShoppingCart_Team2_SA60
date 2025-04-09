b795a654d262f22d95c228b993ddce6e39603122
kinsengchan <chan_kin_seng@hotmail.com>
2025-04-09 16:30:29 +0800

    Change to add cart url and cart button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductList from './pages/ProductList';
 import OrderList from './pages/OrderList';
 import Login from './pages/Login';
+import CartDetails from './pages/CartDetails';
+import { AuthProvider } from './context/AuthContext';
 
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-      <Routes>
-        <Route path='/' exact={true} element={<ProductList />} />
-        <Route path='/login' exact={true} element={<Login />} />
-        <Route path='/orders' exact={true} element={<OrderList />} />
+        <Routes>
+          <Route path='/' exact={true} element={<ProductList />} />
+          <Route path='/login' exact={true} element={<Login />} />
+          <Route path='/orders' exact={true} element={<OrderList />} />
           <Route path='/cart' exact={true} element={<CartDetails />} />
-      </Routes>
+        </Routes>
       </Router>
     </AuthProvider>
   )
