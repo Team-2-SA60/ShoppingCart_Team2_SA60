d89af04e87efb9b280641d3f339c2b9b5c55e2b2
adriantlh <adrian.tan.li.huat@gmail.com>
2025-04-14 19:46:11 +0800

    Adding new arrangement features

--- src/main/java/nus/shoppingcart_team2_sa60/repository/ProductRepository.java
+++ src/main/java/nus/shoppingcart_team2_sa60/repository/ProductRepository.java
@@ -23,6 +23,18 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
             "ORDER BY p.name")
     List<Product> sortProductByName();
 
+    @Query("SELECT p FROM Product p " +
+            "ORDER BY p.name DESC ")   
+    List<Product> sortProductByNameDesc();
+
+    @Query("SELECT p FROM Product p " +
+            "ORDER BY (p.price - p.discount)")
+    List<Product> sortProductByPrice();
+
+    @Query("SELECT p FROM Product p " +
+            "ORDER BY (p.price-p.discount) DESC ")   
+    List<Product> sortProductByPriceDesc();
+
     /*
     @Query("SELECT p " +
             "FROM Product p " +
