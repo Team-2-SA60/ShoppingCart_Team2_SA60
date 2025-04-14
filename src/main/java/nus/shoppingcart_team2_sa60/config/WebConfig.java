177ec3c978709faf7a6ef73624c936ac887fbaf4
kinsengchan <chan_kin_seng@hotmail.com>
2025-04-14 14:29:37 +0800

    Removed unnecessary imports

--- src/main/java/nus/shoppingcart_team2_sa60/config/WebConfig.java
+++ src/main/java/nus/shoppingcart_team2_sa60/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@org.springframework.core.annotation.Order(1)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
