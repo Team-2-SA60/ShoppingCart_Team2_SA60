6771e0d9546c6444fb104c058b4210ae52f68700
im-ksc <chan_kin_seng@hotmail.com>
2025-04-19 19:07:40 +0800

    Minor bug fix for credit card expiry month (cannot be < 1 or > 13)

--- src/main/java/nus/shoppingcart_team2_sa60/validator/CreditCardValidator.java
+++ src/main/java/nus/shoppingcart_team2_sa60/validator/CreditCardValidator.java
@@ -16,6 +16,13 @@ public class CreditCardValidator implements Validator {
     public void validate(Object target, Errors errors) {
         CreditCardDTO creditCard = (CreditCardDTO) target;
 
+        int expiryMonth = Integer.parseInt(creditCard.getCreditCardExpiryMonth());
+
+        if (expiryMonth < 1 || expiryMonth > 12) {
+            errors.rejectValue("creditCardExpiry", "error.creditCardExpiry", "Expiry month must be between 1 and 12");
+            return;
+        }
+
         if (creditCard.isExpired()) {
             errors.rejectValue("creditCardExpiry", "error.creditCardExpiry", "Credit Card has already expired");
         }
